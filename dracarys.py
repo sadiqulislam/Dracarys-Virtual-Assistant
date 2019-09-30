7d0d0d8b25768dffd01fa84d0cef400d9ee5e234
Shishir <sishishir2015@gmail.com>
2019-09-30 22:42:19 +0600

    Wish Me Function Complete .This Function Wish Me Hour

--- dracarys.py
+++ dracarys.py
@@ -4,9 +4,9 @@ import datetime
 engine = pyttsx3.init('sapi5')
 
 voices = engine.getProperty('voices')
-print(voices[0].id)
+#print(voices[0].id)
 
-engine.setProperty('voice',voices[0].id)
+engine.setProperty('voice',voices[2].id)
 
 def speak(audio):
     engine.say(audio)
@@ -18,6 +18,11 @@ def wishme():
         speak("Good Morning,Presence")
     elif hour>=12 and hour<16:
         speak("Good Noon,Presence")
-    elif 
+    elif hour>=16 and hour<19:
+        speak("Good Afternoon,Presence")
+    else:
+        speak("Good Night,Presence")
+    speak("I Am Dracarys,Sir! What may I Do Now,Sir?")
 if __name__ == '__main__':
-    speak("I am Dracrys And I Served Presence")
+    #speak("I am Dracarys And I Served Presence")
+    wishme()
